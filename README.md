1b2b77473b0273b118169c8f5f7269fed41a9d32
Mika-Pikazo <1376106928@qq.com>
2018-07-05 00:21:25 +0800

    jason: Initial jason tree push

---
+++ README.md
@@ -0,0 +1,26 @@
+Copyright (C) 2017 - 2018 The LineageOS Project
+
+Device configuration for Xiaomi Mi Note 3
+=========================================
+
+The Xiaomi Mi Note 3 (codenamed _"Jason"_) is a high-end smartphone from Xiaomi.
+
+It was announced in September 2017. Release date was June 2018.
+
+## Device specifications
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | Quad-core 2.2GHz Kryo 260 & quad-core 1.8GHz Kryo 260
+Chipset | Qualcomm SDM660 Snapdragon 660
+GPU     | Adreno 512
+Memory  | 4 GB RAM
+Shipped Android Version | 7.1.2
+Storage | 32/64 GB (EMMC Flash)
+Battery | Non-removable Li-Po 3500 mAh (QC 3.0)
+Display | 1080 x 1920 pixels, 5.5 inches (~403 ppi pixel density)
+Camera  | Dual 12 MP, f/2.0, phase detection autofocus, dual-LED (dual tone) flash
+
+## Device Picture
+
+![Xiaomi Mi Note 3](https://scdn.androidcommunity.com/wp-content/uploads/2017/09/xiaomi-mi-note3-1.jpg "Xiaomi Mi Note 3")
---
+++ lineage.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Release name
+PRODUCT_RELEASE_NAME := jason
+
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+# Inherit from jason device
+$(call inherit-product, device/xiaomi/jason/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_jason
+PRODUCT_DEVICE := jason
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi Note 3
+
+TARGET_VENDOR_PRODUCT_NAME := jason
+TARGET_VENDOR_DEVICE_NAME := jason
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=jason PRODUCT_NAME=jason
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 8.6.26 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/jason/jason:8.1.0/OPM1.171019.019/8.7.3:user/release-keys"
