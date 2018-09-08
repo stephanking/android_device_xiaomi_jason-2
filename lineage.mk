cea562a3a42fc796aa52238fc544c75a0cf17cff
litakasumi <1376106928@qq.com>
2018-09-08 10:22:59 +0800

    jason: update fingerprint from V10.0.2.0.OCHCNFH

--- lineage.mk
+++ lineage.mk
@@ -46,7 +46,7 @@ TARGET_VENDOR_DEVICE_NAME := jason
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=jason PRODUCT_NAME=jason
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 8.8.29 release-keys"
+    PRIVATE_BUILD_DESC="jason-user 8.1.0 OPM1.171019.019 V10.0.2.0.OCHCNFH release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/jason/jason:8.1.0/OPM1.171019.019/8.8.29:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/jason/jason:8.1.0/OPM1.171019.019/V10.0.2.0.OCHCNFH:user/release-keys"
